25390d88694bc8e4b123444667dc27a5173271fc
Kumvijaya <Kumvijaya@gmail.com>
2023-07-31 01:15:23 +0530

    added additional test suites

--- src/test/java/com/mycompany/app/Test1.java
+++ src/test/java/com/mycompany/app/Test1.java
@@ -4,7 +4,7 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;		
 
-public class SuiteTest1 {						
+public class Test1 {						
 
     @Test				
     public void testJUnitMessage() {				
