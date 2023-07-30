ef68f73f7792a0f49a7fe0f15523e0d31ff5ac5f
Kumvijaya <Kumvijaya@gmail.com>
2023-07-31 00:32:09 +0530

    added additional test suites

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -1,8 +1,12 @@
 package com.mycompany.app;
 
-import org.junit.jupiter.api.Test;
+// import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import static org.junit.Assert.assertEquals;				
+
+import org.junit.Test;
 
 /**
  * Unit test for simple App.
